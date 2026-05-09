14f34d3e355bdc06cd9db660ca4d165bf1ca3438
Arben Apura <arben.m.apura@gmail.com>
2026-05-09 11:27:40 +0800

    feat(seo): add reusable Breadcrumbs JSON-LD component

---
+++ src/lib/components/seo/Breadcrumbs.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	// IMPORTED COMPONENTS
+	import JsonLd from '$lib/components/seo/JsonLd.svelte';
+
+	// -- REQUIRED PROPS -- //
+
+	export let pageName: string;
+	export let pageUrl: string;
+</script>
+
+<JsonLd
+	schema={{
+		'@context': 'https://schema.org',
+		'@type': 'BreadcrumbList',
+		itemListElement: [
+			{
+				'@type': 'ListItem',
+				position: 1,
+				name: 'Home',
+				item: 'https://arbenger.com',
+			},
+			{
+				'@type': 'ListItem',
+				position: 2,
+				name: pageName,
+				item: pageUrl,
+			},
+		],
+	}}
+/>
